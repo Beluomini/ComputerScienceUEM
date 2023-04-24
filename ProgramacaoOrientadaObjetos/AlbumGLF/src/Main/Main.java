dfb7e13d847b99e84c75a6f18092e63eee4cbb3c
Beluomini <lucasbeluomini1@gmail.com>
2023-04-24 15:28:44 -0300

    AlbumGLF figurinhas copa POO 4Semestre

---
+++ ProgramacaoOrientadaObjetos/AlbumGLF/src/Main/Main.java
@@ -0,0 +1,23 @@
+package Main;
+
+//Alunos:
+//Lucas Beluomini 120111
+//Feilpe Chatalov 118992
+//Gabriel Rodrigues 118038
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+
+    public static void main(String[]args) throws SQLException {
+        GUILogin gui = new GUILogin();
+    }
+    
+}
